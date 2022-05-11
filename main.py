b9ac152c0205ed5bbd170dcebefd93c616bc97ce
Vinishk0 <94686362+Vinishk0@users.noreply.github.com>
2022-05-12 01:18:03 +0500

    Дни недели + фикс бага

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sqlite3
 import openpyxl
 import datetime
 
-bot = Bot(token='5357337704:AAHV-MOPCy2SHgJJiWuatDV0Oa8YEyar5aM')
+bot = Bot(token='5357337704:AAFW4d8Yq3Gwp3-2J0oOMmyIzrwJSbUVu8c')
 dp = Dispatcher(bot)
 
 connection = sqlite3.connect('Users.db')
@@ -16,7 +16,8 @@ sheets = wb.sheetnames
 sheet = wb[sheets[0]]
 fixed_schedule = False
 
-week = {0: (6, 12), 1: (12, 18), 2: (18, 24), 3: (24, 30), 4: (30, 360), 5: (36, 42), 6: (42, 48)}
+week_days = {'Понедельник': 0, 'Вторник': 1, 'Среда': 2, 'Четверг': 3, 'Пятница': 4, 'Суббота': 5, 'Воскресенье': 6}
+week = {0: (6, 12), 1: (12, 18), 2: (18, 24), 3: (24, 30), 4: (30, 36), 5: (36, 42), 6: (42, 48)}
 groups = {'1бд1': 'C', '1бд3': 'D', '1бу1': 'E', '1бу2': 'F', '1бу3': 'G', '1вб1': 'H', '1вб2': 'I', '1вб3': 'J',
           '1ис1': 'K', '1ис3': 'L', '1пк1': 'M', '1пк2': 'N', '1са1': 'O', '1са3': 'P'}
 
@@ -37,14 +38,6 @@ async def help(message: types.Message):
     await bot.send_message(chat_id=message.chat.id, text="Я бот, который будет скидыват тебе рассписание каждый день!")
 
 
-@dp.message_handler(text=['Изменить группу', 'Поменять группу', 'Выбрать группу'])
-async def edit_group(message: types.Message):
-    await bot.send_message(chat_id=message.chat.id, text="Напишите, на какую группу вы хотите изменить")
-    cur.execute(f'''UPDATE user_info
-                        SET groups = 'None'
-                        WHERE chat_id = {message.chat.id} ''')
-
-
 @dp.message_handler(text=['Изменить расписание', 'Поменять расписание'])
 async def edit_schedule(message: types.Message):
     global fixed_schedule
@@ -57,13 +50,43 @@ async def edit_schedule(message: types.Message):
         await bot.send_message(chat_id=message.chat.id, text="Вы не являетесь администратором!")
 
 
-@dp.message_handler(text=['Расписание', 'Рассписание на завтра'])
+@dp.message_handler(text=['Завтра', 'Рассписание на завтра'])
 async def schedule(message: types.Message):
     text = ''
     try:
         cort = week[datetime.datetime.today().weekday() + 1]
     except IndexError:
         cort = week[0]
+    group = groups[cur.execute(f'''select groups From user_info
+                                    where chat_id = {message.chat.id} ''').fetchall()[0][0]]
+    for i in range(*cort):
+        cell = sheet[f'{group}{str(i)}']
+        if str(cell.value) == 'None':
+            text += f'Пара №{abs(cort[0] - i) + 1} ----------- \n'
+        else:
+            text += f'Пара №{abs(cort[0] - i) + 1} {str(cell.value)}\n'
+    await bot.send_message(chat_id=message.chat.id, text=text)
+
+
+@dp.message_handler(text=['Расписание', 'Рассписание на сегодня'])
+async def schedule(message: types.Message):
+    text = ''
+    cort = week[datetime.datetime.today().weekday()]
+    group = groups[cur.execute(f'''select groups From user_info
+                                    where chat_id = {message.chat.id} ''').fetchall()[0][0]]
+    for i in range(*cort):
+        cell = sheet[f'{group}{str(i)}']
+        if str(cell.value) == 'None':
+            text += f'Пара №{abs(cort[0] - i) + 1} ----------- \n'
+        else:
+            text += f'Пара №{abs(cort[0] - i) + 1} {str(cell.value)}\n'
+    await bot.send_message(chat_id=message.chat.id, text=text)
+
+
+@dp.message_handler(text=['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'])
+async def schedule(message: types.Message):
+    text = ''
+    cort = week[week_days[message.text]]
     group = groups[cur.execute(f'''select groups From user_info
                                     where chat_id = {message.chat.id} ''').fetchall()[0][0]]
     for i in range(*cort):
